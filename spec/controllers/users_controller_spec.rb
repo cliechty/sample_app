68b969b2def9528620e12cee306d43893746a264
Chris Liechty <evilhomer78@gmail.com>
2012-01-28 14:25:38 -0700

    Chapter 8 exercises

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -44,6 +44,26 @@ describe UsersController do
 			get :new
 			response.should have_selector("title", :content => "Sign up")
 		end
+
+		it "should have a name field" do
+			get :new
+			response.should have_selector("input[name='user[name]'][type='text']")
+		end
+
+		it "should have an email field" do
+			get :new
+			response.should have_selector("input[name='user[email]'][type='text']")
+		end
+
+		it "should have a password field" do
+			get :new
+			response.should have_selector("input[name='user[password]'][type='password']")
+		end
+
+		it "should have a confirm password field" do
+			get :new
+			response.should have_selector("input[name='user[password_confirmation]'][type='password']")
+		end
 	end
 
 	describe "POST 'create'" do
@@ -67,6 +87,12 @@ describe UsersController do
 			it "should render the 'new' page" do
 				post :create, :user => @attr
 				response.should render_template('new')
+			end
+
+			it "should clear the 'password' field" do
+				@attr.merge(:password => "xxx")
+				post :create, :user => @attr
+				response.should have_selector("input[name='user[password]']", :content => "")
 			end
 		end
 
